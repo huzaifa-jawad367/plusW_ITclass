d4802e8a51ddd58a91cf099476b956e65fdc04a4
huzaifa-jawad367 <hjawad.bscs21seecs@seecs.edu.pk>
2025-02-25 00:37:56 +0500

    Added task 1 screenshots

---
+++ Class5/Assignment/Readme.md
@@ -0,0 +1,8 @@
+# Assignment 1
+## Task 1
+
+![alt text](Assets/image.png)
+
+![alt text](Assets/executed_code1.png)
+
+![alt text](Assets/image-1.png)
--- Class5/Assignment/Task1/Task1.py
+++ Class5/Assignment/Task1/Task1.py
@@ -4,36 +4,36 @@ import shutil
 import pandas as pd
 
 # Ensure the backup folder exists
-backup_folder = "backup_folder"
+backup_folder = "Class5/backup_folder"
 if not os.path.exists(backup_folder):
     os.makedirs(backup_folder)
 
 # Move CSV files to the backup folder
-csv_files = glob.glob("*.csv")
+csv_files = glob.glob("./Class5/csv_files/*.csv")
 for file in csv_files:
     shutil.move(file, backup_folder)
     print(f"Moved file: {file}")
 
-# Function to export data in different formats
-def export_data(df, filename, format):
-    if format == "csv":
-        df.to_csv(filename, index=False)
-        print(f"Data exported to {filename} in CSV format.")
-    elif format == "json":
-        df.to_json(filename, orient="records")
-        print(f"Data exported to {filename} in JSON format.")
-    else:
-        print("Unsupported format.")
+# # Function to export data in different formats
+# def export_data(df, filename, format):
+#     if format == "csv":
+#         df.to_csv(filename, index=False)
+#         print(f"Data exported to {filename} in CSV format.")
+#     elif format == "json":
+#         df.to_json(filename, orient="records")
+#         print(f"Data exported to {filename} in JSON format.")
+#     else:
+#         print("Unsupported format.")
 
-# Sample DataFrame
-data = {
-    'Name': ['Alice', 'Bob', 'Charlie'],
-    'Age': [25, 30, 35],
-    'City': ['New York', 'Los Angeles', 'Chicago']
-}
+# # Sample DataFrame
+# data = {
+#     'Name': ['Alice', 'Bob', 'Charlie'],
+#     'Age': [25, 30, 35],
+#     'City': ['New York', 'Los Angeles', 'Chicago']
+# }
 
-df = pd.DataFrame(data)
+# df = pd.DataFrame(data)
 
-# Export data
-export_data(df, "output.csv", "csv")
-export_data(df, "output.json", "json")
+# # Export data
+# export_data(df, "output.csv", "csv")
+# export_data(df, "output.json", "json")
